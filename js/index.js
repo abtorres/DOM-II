5e99972267fe73dbd4c5a2a6e07183fcbcd6f45d
abtorres <abtorres079@gmail.com>
2019-04-13 12:17:54 -0600

    having issues getting the nav to change color

--- js/index.js
+++ js/index.js
@@ -20,8 +20,18 @@ btns.forEach(e => {
     });
 });
 
-let bodyColor = document.querySelector('body');
-bodyColor.addEventListener('keydown', e => {
-    e.target.style.background = 'lightyellow';
+
+//nav changes color
+let navColor = document.querySelector('.container');
+navColor.addEventListener('keydown', e => {        //does not work ???
+    e.target.style.background = 'yellow';
+    console.log(e) //nothing
 });
+//body changes color when key pressed 
+let bodyColor = document.querySelector('body');
+bodyColor.addEventListener('keydown', e => {       //this works
+    e.target.style.background = 'lightyellow';
+    console.log(e) //returns: keydown {target: body, key: "?", charcode: ?, keyCode: ?}
+});
+
 
